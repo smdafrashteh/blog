650bf65001ab82cae947dd5eb2f671c745ce6767
alikazemi76 <ali.kazemi@yahoo.com>
2022-07-26 18:22:43 +0430

    space

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
+
                     @endif
 
                     {{ __('You are logged in!') }}
